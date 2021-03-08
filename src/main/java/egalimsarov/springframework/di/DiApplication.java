eda4946b7016e11377083cd82125c85c78d21968
Evgenii <e.galimsarov@gmail.com>
2021-03-08 20:25:04 +0500

    Primary Beans

--- src/main/java/egalimsarov/springframework/di/DiApplication.java
+++ src/main/java/egalimsarov/springframework/di/DiApplication.java
@@ -14,8 +14,8 @@ public class DiApplication {
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(DiApplication.class, args);
 		MyController myController = (MyController) ctx.getBean("myController");
-		String greeting = myController.sayHello();
-		System.out.println(greeting);
+		System.out.println("------ Primary Bean");
+		System.out.println(myController.sayHello());
 
 		System.out.println("------ Property");
 		PropertyInjectedController propertyInjectedController =
